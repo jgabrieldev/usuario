dd1d1c88e9b0330a5f74dc044e81020136b82f6b
João Gabriel <jgabriel.lourencco@gmail.com>
2025-12-27 13:59:20 -0500

    Add spring security classes

---
+++ src/main/java/com/jgabriel/usuario/infrastructure/entity/Endereco.java
@@ -0,0 +1,31 @@
+package com.jgabriel.usuario.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "endereco")
+public class Endereco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "rua")
+    private String rua;
+    @Column(name = "numero")
+    private Long numero;
+    @Column(name = "complemento", length = 10)
+    private String complemento;
+    @Column(name = "cidade")
+    private String cidade;
+    @Column(name = "estado", length = 2)
+    private String estado;
+    @Column(name = "cep", length = 9)
+    private String cep;
+}
